1c114864af63349615d4e941485933b701b70097
ejayxurpas <ejay.xurpas@gmail.com>
2023-10-07 10:17:46 +0800

    Feat: Add Start Page
    Add tailwind styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,54 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState("");
+
+  const gridStyles = [
+    "auto-rows-[5rem]",
+    "gap-8",
+    "grid",
+    "items-center",
+    "justify-center",
+  ].join(" ");
+
+  const headerStyles = ["font-bold", "font-poppins", "text-8xl"].join(" ");
+
+  const inputStyles = [
+    "bg-transparent",
+    "border-solid",
+    "border-black",
+    "border-[1.5px]",
+    "h-24",
+    "w-[30rem]",
+    "placeholder: text-white",
+    "placeholder: font-bold",
+    "placeholder: font-poppins",
+    "placeholder: p-2",
+    "placeholder: text-6xl",
+  ].join(" ");
+
   return (
-    <div>
-      <span className="font-helvetica text-9xl font-black"> EJAY SARANGAY </span>
+    // this div sets the grid to be centered
+    <div className="flex h-full justify-center items-center">
+      {/* this holds the grid styles */}
+      <div className={gridStyles}>
+        <span className={headerStyles}> Hey, </span>
+        <input
+          type="text"
+          className={`${inputStyles} `}
+          placeholder="your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button
+          className="font-poppins text-2xl font-bold text-center disabled:opacity-50 transition ease-in-out 1000ms"
+          disabled={!name}
+        >
+          {" "}
+          let's go.{" "}
+        </button>
+      </div>
     </div>
   );
 }
